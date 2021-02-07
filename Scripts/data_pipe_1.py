ccc9eef7952f5afd3da8e612475a8a7f6f95cd07
Jorid Topi <jorid.topi@gmail.com>
2021-02-07 14:10:57 -0500

    Changed pipeline order for more efficiency

--- Scripts/data_pipe_1.py
+++ Scripts/data_pipe_1.py
@@ -7,8 +7,7 @@ Created on Sun Feb  7 10:05:56 2021
 
 This is the priority 1 data pipeline, which is used for the primary objectives of the analysis.
 The variable analysis lookup is used to keep only the variables that were identified as useful
-for this analysis. The priority 2 and 3 variables are dropped, as well as variables that are 
-no longer useful after being transformed.
+for this analysis. The priority 2 and 3 variables are dropped.
 
 """
 
@@ -16,12 +15,6 @@ import pandas as pd
 
 #Read output of first data cleanup
 nhanes = pd.read_pickle('../Data/nhanes_pre_proc.pkl')
-
-
-#Drop the variables that are now transformed. This is identified in the data_read.py
-
-#New variables added in data_read
-transformed_vars = ['SDDSRVYR', 'DR1.030Z', 'DR1.020']
 
 
 #Use the variable lookup to filter out variables for reducing the dataset
@@ -32,8 +25,7 @@ var_pri_2 = variable_lookup[variable_lookup['Priority'] == 2]
 var_pri_3 = variable_lookup[variable_lookup['Priority'] == 3]
 
 
-#Drop the transformed and priority 2 and 3 variables
-nhanes = nhanes.drop(transformed_vars, axis = 1)
+#Drop the priority 2 and 3 variables
 nhanes = nhanes.drop(var_pri_2['Variable'], axis = 1)
 nhanes = nhanes.drop(var_pri_3['Variable'], axis = 1)
 
--- Scripts/data_read.py
+++ Scripts/data_read.py
@@ -3,7 +3,11 @@
 """
 Created on Sat Jan 30 11:12:05 2021
 
-@author: jori
+@author: Jorid Topi
+
+This script reads the original NHANES .csv data file and drops the variables deemed to irrelevant
+for the objectives of the analysis, using variable priority assignment lookup table.
+
 """
 
 import pandas as pd
@@ -16,67 +20,11 @@ variable_lookup = pd.read_csv('../Analysis/Variable_Analysis_Lookup.csv')
 #Pull out the priority 0 variables, which are dropped from the start
 var_pri_0 = variable_lookup[variable_lookup['Priority'] == 0]
 
-
 #The following variables have been deemed irrelevant for this analysis, so they are dropped.
 nhanes = nhanes.drop(var_pri_0['Variable'], axis = 1)
 
 
-
-#Map the survey year data, based on the SDDSRVYR encoding key
-
-#Obtain description and value counts
-nhanes['SDDSRVYR'].describe()
-nhanes['SDDSRVYR'].value_counts()
-
-#Create Survey Year variable based on lookup, mapping from CDC source
-survey_year_lookup = {4: '2005-2006', 5: '2009-2010', 6: '2011-2012', 7: '2013-2014', 8:'2015-2016', 9:'2017-2018'}
-nhanes['Survey_Year'] = nhanes['SDDSRVYR'].map(survey_year_lookup)
-#nhanes = nhanes.drop(['SDDSRVYR'], axis = 1)
-
-#Check for NAs
-#print("Survey Year NA count is "+str(nhanes['Survey_Year'].isnull().sum()))
-
-
-#Map the meal occasion data, based on the DR1.030Z encoding key
-
-#Obtain description and value counts
-nhanes['DR1.030Z'].describe()
-nhanes['DR1.030Z'].value_counts()
-
-#Create Survey Year variable based on lookup, mapping from CDC source
-meal_name_lookup = {1: 'Breakfast', 2: 'Lunch', 3: 'Dinner', 4: 'Supper', 5:'Brunch', 6:'Snack',
-                    7: 'Drink', 8: 'Infant Feeding', 9: 'Extended consumption', 10: 'Desayano',
-                    11: 'Almuerzo', 12: 'Comida', 13: 'Merienda', 14: 'Cena', 15: 'Enter comida',
-                    16: 'Botana', 17: 'Bocadillo', 18: 'Tentempie', 19: 'Bebida', 91: 'Other'}
-
-
-nhanes['Meal_Name'] = nhanes['DR1.030Z'].map(meal_name_lookup)
-#nhanes = nhanes.drop(['DR1.030Z'], axis = 1)
-
-#Check for NAs
-#print("Meal Name NA count is "+str(nhanes['Meal_Name'].isnull().sum()))
-
-
-#Create a time column, in a pandas time format
-
-#Remove the 5AM bias from the value in seconds
-def remove_time_bias(time_in):
-    midnight = 24*60*60
-    if (time_in >= midnight):
-        time_post = time_in - midnight
-    else: time_post = time_in
-    return round(time_post)
-
-#Create time variable and convert to time formatefrom DR1.020
-nhanes['Time'] = nhanes['DR1.020'].apply(remove_time_bias)
-nhanes['Time'] = nhanes['Time'].astype(int)
-nhanes['Time'] = nhanes['Time'].round().apply(pd.to_timedelta, unit='s')
-#nhanes = nhanes.drop(['DR1.020'], axis = 1)
-
-
-
-
-
+#Save data file 
 nhanes.to_pickle('../Data/nhanes_pre_proc.pkl')
 
 
