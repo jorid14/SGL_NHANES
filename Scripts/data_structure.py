64982fc41b964a0a4e0b5495117f7b32001fe56e
Jorid Topi <jorid.topi@gmail.com>
2021-02-12 13:22:10 -0500

    extract corpus for side dishes and seafood meals

--- Scripts/data_structure.py
+++ Scripts/data_structure.py
@@ -11,11 +11,30 @@ This script performs the data structuring tasks as identified by the project obj
 """
 
 import pandas as pd
+import re
+import nltk
 
+
+#Read filtered dataframe
 nhanes = pd.read_pickle('../Data/nhanes_post.pkl')
 
 
+#Obtain dataframe with seafood items
+seafood_df = nhanes[nhanes['DR1I_PF_SEAFD_TOT'] > 0]
+#Obtain dataframe with side dishes
+side_dish_df = nhanes[nhanes['DR1I_PF_SEAFD_TOT'] == 0]
 
+"""
+Obtain initial test corpus for the whole meal, seafood item only, and side dishes only
+Obtains the first word in the text description string before a comma, if comma exists.
+Obtains the whole string in the text description if comma is not present.
+"""
+food_type_cps = nhanes['DESCRIPTION'].apply(lambda x: re.search(r'^([^,])+', x).group(0) if re.search((r','), x) else x)
+seafood_cps = seafood_df['DESCRIPTION'].apply(lambda x: re.search(r'^([^,])+', x).group(0) if re.search((r','), x) else x)
+side_dish_cps = side_dish_df['DESCRIPTION'].apply(lambda x: re.search(r'^([^,])+', x).group(0) if re.search((r','), x) else x)
 
+#Obtain and plot frequency distribution of the side dish words
+side_dish_fdist = nltk.FreqDist(side_dish_cps)
+side_dish_fdist.plot(30)
 
 
