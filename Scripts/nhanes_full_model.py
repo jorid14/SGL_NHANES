0d5b7c30ded685be0f792d88c9fa3fb11b14927d
jorid14 <jorid.topi@gmail.com>
2021-03-30 23:06:24 -0400

    clean up of log reg model script

--- Scripts/nhanes_full_model.py
+++ Scripts/nhanes_full_model.py
@@ -3,13 +3,21 @@
 """
 Created on Sun Mar 28 20:10:11 2021
 
-@author: jori
 """
 
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn import linear_model
+from itertools import combinations
 import numpy as np
 import scipy.stats as stat
+import sys
+import time 
+
+
+
 
 class LogisticReg:
     """
@@ -49,86 +57,170 @@ class LogisticReg:
 
 
 
+def nhanes_full_log_reg(df, fped_vars, var_combinatorial, batch_run, batch_num, 
+                        batch_step, non_sfd_class_n, sfd_class_n, test_ratio):
+    
+    #Model execution start time
+    startTime = time.time()
+    #Create list of all FPED variable combinations if desired    
+    if (var_combinatorial == True):
+        cmb_output = sum([list(map(list, combinations(fped_vars , i))) for i in range(len(fped_vars ) + 1)], [])
+        cmb_output = cmb_output[1:]
+    else: 
+        cmb_output = fped_vars
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+    #Partition the list of FPED variable combinations with batch parameters, if batch run desired
+    if (batch_run == True):
+        #Obtain batch length and step through the indecies of the variable list
+        batch_len = int(len(cmb_output)/batch_num)
+        idx1 = 0 + batch_len*int(batch_step)
+        idx2 = batch_len + batch_len*int(batch_step)
+        cmb_output = cmb_output[idx1:idx2]
 
-df = pd.read_pickle('../Data/nhanes_full_pre_proc.pkl')
+    #This is the model fitting loop if combinatorial variable model selection is desired
+    if (var_combinatorial == True):
+        #Lists for storing the model prediction success rate and variable list
+        pred_sr = []
+        var_list = []
+        #Loops through the generate variable combinations
+        for var in cmb_output:
+            #Sample the seafood and non-seafood classes, create model input df
+            df_non_sfd = df[df['seafood_meal']==0].sample(n=non_sfd_class_n)
+            df_sfd = df[df['seafood_meal']==1].sample(n=sfd_class_n)
+            df_mdl = pd.concat([df_non_sfd, df_sfd])
+            #Add the classification target variable to the df input list
+            var.append('seafood_meal')
+            #Use variable combination selected by loop
+            df_mdl = df_mdl[var]
+            #Split the training and test data
+            X_train, X_test, y_train, y_test = train_test_split(df_mdl.drop(['seafood_meal'], axis=1), df_mdl['seafood_meal'], test_size=test_ratio)
+            #Fit the logistic regression model
+            log_reg = LogisticRegression()
+            log_reg.fit(X_train, y_train)
+            #Obtain predictions on test set and calculate success rate
+            y_pred = log_reg.predict(X_test)
+            n_correct = sum(y_pred == y_test)
+            sr = n_correct/len(y_pred)
+            #Add success rate and variables used to list for storing outside loop
+            pred_sr.append(sr)
+            var_list.append(var)
+        
+        #Calculate model execution time for combinatorial variable selection    
+        cmb_time = time.time() - startTime  
+        cmb_time_list = [cmb_time] * len(cmb_output)
+        cmb_time_df = pd.DataFrame(cmb_time_list)
+        cmb_time_df = cmb_time_df.rename({0: 'Result Runtime (Seconds)'}, axis=1)
+        #Create a dataframe with variables used, their success rate , and runtime   
+        pred_sr_df = pd.DataFrame(pred_sr)
+        pred_sr_df = pred_sr_df.rename({0: 'Success Rate'}, axis=1)
+        var_list_df = pd.DataFrame(var_list)
+        model_result = pd.concat([var_list_df, pred_sr_df, cmb_time_df], axis=1)
+        
+        
+        
+    #Condition if variable combination is not desired  
+    else:
+        #Sample the seafood and non-seafood classes, create model input df
+        df_non_sfd = df[df['seafood_meal']==0].sample(n=non_sfd_class_n)
+        df_sfd = df[df['seafood_meal']==1].sample(n=sfd_class_n)
+        df_mdl = pd.concat([df_non_sfd, df_sfd])
+        #Add the classification target variable to the df input list
+        fped_vars.append('seafood_meal')
+        #Use variable combination selected by loop
+        df_mdl = df_mdl[fped_vars]
+        #Split the training and test data
+        X_train, X_test, y_train, y_test = train_test_split(df_mdl.drop(['seafood_meal'], axis=1), df_mdl['seafood_meal'], test_size=test_ratio)
+        #Fit the logistic regression model
+        log_reg = LogisticRegression()
+        log_reg.fit(X_train, y_train)
+        #Obtain predictions on test set and calculate success rate
+        y_pred = log_reg.predict(X_test)
+        n_correct = sum(y_pred == y_test)
+        pred_sr = [str(n_correct/len(y_pred))]
+        #Calculate model execution time for combinatorial variable selection    
+        non_cmb_time = time.time() - startTime  
+        non_cmb_time_df = pd.DataFrame([non_cmb_time ])
+        non_cmb_time_df = non_cmb_time_df.rename({0: 'Result Runtime (Seconds)'}, axis=1)
+        #Create a dataframe with variables used and their success rate    
+        pred_sr_df = pd.DataFrame(pred_sr)
+        pred_sr_df = pred_sr_df.rename({0: 'Success Rate'}, axis=1)
+        var_list_df = pd.DataFrame([fped_vars])
+        model_result = pd.concat([var_list_df, pred_sr_df, non_cmb_time_df], axis=1)
+    
+    #Returns result from model.     
+    return model_result
+ 
+
+#Contains list of all FPED variable plus seafood meal classification target
+all_vars = ['F_CITMLB', 'F_OTHER', 'F_JUICE', 'F_TOTAL', 
+            'V_DRKGR', 'V_REDOR_TOMATO', 'V_REDOR_OTHER', 'V_REDOR_TOTAL', 
+            'V_STARCHY_POTATO', 'V_STARCHY_OTHER', 'V_STARCHY_TOTAL', 'V_OTHER', 
+            'V_TOTAL', 'V_LEGUMES', 
+            'G_WHOLE', 'G_REFINED', 'G_TOTAL', 
+            'PF_EGGS', 'PF_SOY', 'PF_NUTSDS', 'PF_LEGUMES', 
+            'D_MILK', 'D_YOGURT', 'D_CHEESE', 'D_TOTAL', 
+            'OILS', 'SOLID_FATS', 'ADD_SUGARS', 'A_DRINKS', 
+            'seafood_meal']
 
 
 #Create a list of the high level food components, as defined in the FPED
 #Fruit, Vegetables, Grains, Protein Foods, and Dairy components
-food_cmp_level1 = [
-    'F_TOTAL',
-    'V_TOTAL',
-    'G_TOTAL',
-    'D_TOTAL',
-    'seafood_meal']
+#Include oils, fats, and sugars at this level.
+food_cmp_level1 = ['F_TOTAL','V_TOTAL','G_TOTAL','D_TOTAL','OILS', 
+                   'SOLID_FATS', 'ADD_SUGARS']
 
-food_cmp_oils_fats = [
-    'OILS',
-    'SOLID_FATS',
-    'PF_SEAFD_TOT']
 
-pvs = []
-log1_pred_sr = []
+#Level 2 contains the subcomponents of vegetables, grains, 
+#proteins other than meat and seafood, dairy.
+#Keep fruits at total level
+#Include oils, fats, and sugars at this level.
+food_cmp_level2 = ['F_TOTAL', 
+                   'V_DRKGR', 'V_REDOR_TOMATO','V_REDOR_OTHER', 'V_STARCHY_POTATO', 
+                   'V_STARCHY_OTHER', 'V_OTHER', 'V_LEGUMES', 
+                   'G_WHOLE','G_REFINED', 
+                   'PF_EGGS', 'PF_SOY', 'PF_NUTSDS', 'PF_LEGUMES', 
+                   'D_MILK', 'D_YOGURT','D_CHEESE', 
+                   'OILS', 'SOLID_FATS', 'ADD_SUGARS'] 
 
-for i in range(10):
-    df_non_sfd = df[df['seafood_meal']==0].sample(frac=0.061480111199528745)
-    df_sfd = df[df['seafood_meal']==1]
-    df = pd.concat([df_non_sfd, df_sfd])
+#Level 3 has all components of level2, but breaks the total fruit into subcomponents
+food_cmp_level3 = ['F_CITMLB', 'F_OTHER', 'F_JUICE', 
+                   'V_DRKGR', 'V_REDOR_TOMATO', 'V_REDOR_OTHER', 'V_STARCHY_POTATO', 
+                   'V_STARCHY_OTHER', 'V_OTHER', 'V_LEGUMES', 
+                   'G_WHOLE','G_REFINED', 
+                   'PF_EGGS', 'PF_SOY', 'PF_NUTSDS', 'PF_LEGUMES', 
+                   'D_MILK', 'D_YOGURT', 'D_CHEESE', 
+                   'OILS', 'SOLID_FATS', 'ADD_SUGARS']    
+
+food_cmp_exp = ['F_TOTAL','V_TOTAL','G_TOTAL', 'V_DRKGR',
+                'V_REDOR_TOMATO','V_REDOR_OTHER', 'V_STARCHY_POTATO']
+
+#If running by script with arguments
+if len(sys.argv) > 1:
+    #Read the pre-processed dataframe.
+    df = pd.read_csv('nhanes_full_pre_proc.csv')
+    model_res_df = nhanes_full_log_reg(df = df,
+                                       fped_vars = food_cmp_exp, 
+                                        var_combinatorial = True, 
+                                        batch_run = False, 
+                                        batch_num = sys.argv[2], 
+                                        batch_step = sys.argv[1], 
+                                        non_sfd_class_n = 1000, 
+                                        sfd_class_n = 1000, 
+                                        test_ratio = 0.2)
     
-    df = df[food_cmp_level1]
+    model_res_df.to_csv('model_res_df_'+sys.argv[1]+'.csv')
+#If running with no script arguments    
+else:
+    #Read the pre-processed dataframe.
+    df = pd.read_csv('../Data/nhanes_full_pre_proc.csv')
+    model_res_df = nhanes_full_log_reg(df = df,
+                                       fped_vars = food_cmp_exp, 
+                                        var_combinatorial = True, 
+                                        batch_run = False, 
+                                        batch_num = 2, 
+                                        batch_step = 1, 
+                                        non_sfd_class_n = 100, 
+                                        sfd_class_n = 100, 
+                                        test_ratio = 0.2)
     
-    X_train, X_test, y_train, y_test = train_test_split(df.drop(['seafood_meal'], axis=1), df['seafood_meal'], test_size=0.2)
     
-    
-    log_reg = LogisticReg()
-    log_reg.fit(X_train, y_train)
-    
-    log_reg1 = LogisticRegression()
-    log_reg1.fit(X_train, y_train)
-    
-    
-    y_pred = log_reg1.predict(X_test)
-    n_correct = sum(y_pred == y_test)
-    sr = n_correct/len(y_pred)
-    log1_pred_sr.append(sr)
-    
-    pvs.append(log_reg.p_values)
-    df = pd.read_pickle('../Data/nhanes_full_pre_proc.pkl')
-
-
-df_train = pd.concat([X_train, y_train], axis=1)
-#df_train['V_TOTAL'].hist(by=df_train['seafood_meal'], bins=150)
-#df_train['G_TOTAL'].hist(by=df_train['seafood_meal'], bins=150)
-#df_train['D_TOTAL'].hist(by=df_train['seafood_meal'], bins=150)
-#df_train['F_TOTAL'].hist(by=df_train['seafood_meal'], bins=150)
-
-pvs_df = pd.DataFrame(pvs)
-pvs_df.columns = X_train.columns.values.tolist()
-log1_pred_sr_df = pd.DataFrame(log1_pred_sr)
-model_metrics = pd.concat([pvs_df, log1_pred_sr_df], axis=1)
-model_metrics = model_metrics.rename({0:'Succes Rate'},axis=1)
-
-#pvs_df.columns = X_train.columns.values.tolist()
-
-#pvs_df.drop([0], axis=1).plot.line()
-
-import matplotlib.pyplot as plt
-
-
-
-v_total_plot = df_train.groupby('seafood_meal')['V_TOTAL'].plot(kind='kde',xlim=(0,3),title='V_TOTAL',legend=True)
-plt.savefig('V_TOTAL.png')
-plt.clf()
-g_total_plot = df_train.groupby('seafood_meal')['G_TOTAL'].plot(kind='kde',xlim=(0,5),title='G_TOTAL',legend=True)
-plt.savefig('G_TOTAL.png')
-plt.clf()
-d_total_plot = df_train.groupby('seafood_meal')['D_TOTAL'].plot(kind='kde',xlim=(0,2),title='D_TOTAL',legend=True)
-plt.savefig('D_TOTAL.png')
-#f_total_plot = df_train.groupby('seafood_meal')['F_TOTAL'].plot(kind='kde',xlim=(0,1),title='F_TOTAL',legend=True)
-
-
-
