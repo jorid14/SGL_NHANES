ce07edd25b8c9e70d704d03a1e76b32f5677c177
jorid14 <jorid.topi@gmail.com>
2021-04-10 12:19:45 -0400

    re-arragend data pre-processing steps and added simpler logistic regression model runs

---
+++ Scripts/nhanes_full_log_reg_mdl.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 11:59:31 2021
+
+@author: Jorid Topi
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import linear_model
+from itertools import combinations
+import numpy as np
+import scipy.stats as stat
+import sys
+import time 
+import regex as re
+
+def nhanes_full_log_reg(df, fped_vars, non_sfd_class_n, sfd_class_n, test_ratio):
+    
+    #Sample the seafood and non-seafood classes, create model input df
+    df_non_sfd = df[df['seafood_meal']==0].sample(n=non_sfd_class_n)
+    df_sfd = df[df['seafood_meal']==1].sample(n=sfd_class_n)
+    df_mdl = pd.concat([df_non_sfd, df_sfd])
+    #Add the classification target variable to the df input list
+    fped_vars.append('seafood_meal')
+    #Use variable combination selected by loop
+    df_mdl = df_mdl[fped_vars]
+    #Split the training and test data
+    X_train, X_test, y_train, y_test = train_test_split(df_mdl.drop(['seafood_meal'], axis=1), df_mdl['seafood_meal'], test_size=test_ratio)
+    #Fit the logistic regression model
+    log_reg = LogisticRegression()
+    log_reg.fit(X_train, y_train)
+    #Obtain predictions on test set and calculate success rate
+    y_pred = log_reg.predict(X_test)
+    n_correct = sum(y_pred == y_test)
+    pred_sr = [str(n_correct/len(y_pred))]
+    #Calculate model execution time for combinatorial variable selection    
+    #non_cmb_time = time.time() - startTime  
+    #non_cmb_time_df = pd.DataFrame([non_cmb_time ])
+    #non_cmb_time_df = non_cmb_time_df.rename({0: 'Runtime(Seconds)'}, axis=1)
+    #Create a dataframe with variables used and their success rate    
+    pred_sr_df = pd.DataFrame(pred_sr)
+    pred_sr_df = pred_sr_df.rename({0: 'Success Rate'}, axis=1)
+    fped_vars.remove('seafood_meal')
+    var_list_df = pd.DataFrame([fped_vars])
+    #model_result = pd.concat([var_list_df, pred_sr_df, non_cmb_time_df], axis=1)
+    model_result = pd.concat([var_list_df, pred_sr_df], axis=1)
+    return model_result
+
+
+#Level 5 has all components of level4, but breaks the total fruit into subcomponents
+food_cmp_level5 = ['F_CITMLB', 'F_OTHER', 'F_JUICE', 
+                   'V_DRKGR', 'V_REDOR_TOMATO', 'V_REDOR_OTHER', 'V_STARCHY_POTATO', 
+                   'V_STARCHY_OTHER', 'V_OTHER', 'V_LEGUMES', 
+                   'G_WHOLE','G_REFINED', 
+                   'PF_EGGS', 'PF_SOY', 'PF_NUTSDS', 'PF_LEGUMES', 
+                   'D_MILK', 'D_YOGURT', 'D_CHEESE', 
+                   'OILS', 'SOLID_FATS', 'ADD_SUGARS', 'A_DRINKS']  
+
+
+
+#Read the pre-processed dataframe.
+df = pd.read_csv('../Data/nhanes_full_pre_proc.csv')
+df = df[(df['meal_energy']=='Low')]
+#df = df[df['eathome']==1]
+df = df[df['age']>18]
+
+
+'''
+Combinations to try:
+    
+    - Age factor: (1) Keep only adults (2) Keep whole population
+    - Meal size: Try all 4 meal size category combinations
+    - Eat home: Meals at home, meals out, all meals
+    - Seafood meal threshold: (1) Keep old option (2) Re-categorize meals with both
+    - Vegetarian option: (1) Include all (2) Exclude vegeterian meals
+'''
+
+
+sr_tot=[]
+for i in range(100):
+    model_res_df_x = nhanes_full_log_reg(df = df,
+                                   fped_vars = food_cmp_level5, 
+                                   non_sfd_class_n = 500, 
+                                   sfd_class_n = 500, 
+                                   test_ratio = 0.2, )
+    sr_tot.append(model_res_df_x['Success Rate'][0])
+
+sr_tot = pd.DataFrame(sr_tot)
+sr_tot = sr_tot[0].astype(float)
+print(sr_tot.describe())
+
+
+
--- Scripts/nhanes_full_pre_proc.py
+++ Scripts/nhanes_full_pre_proc.py
@@ -9,44 +9,42 @@ Created on Sun Feb  7 10:05:56 2021
 """
 
 import pandas as pd
-import re
+import regex as re
+
+#Read the NHANES full dataset
+nhanes_full = pd.read_csv('../Data/nhanes_full.csv')
+
+'''
+Step #1: Remove columns that are not required, using the variable analysis lookup
+Original data set has 823012x86
+Transformed data set has 823012x50
+'''
+#Use the variable lookup to filter out variables for reducing the dataset
+variable_lookup_full = pd.read_csv('../Analysis/Variable_Analysis_Lookup_NHANES_full.csv')
+#Pull out the priority 0 variables, which are dropped from the start
+var_full_pri_0 = variable_lookup_full[variable_lookup_full['Priority'] == 0]
+#Pull out the priority 2 variables, which are dropped from the start
+var_full_pri_2 = variable_lookup_full[variable_lookup_full['Priority'] == 2]
+#Pull out the priority 3 variables, which are dropped from the start
+var_full_pri_3 = variable_lookup_full[variable_lookup_full['Priority'] == 3]
+
+#The following variables have been deemed irrelevant for this analysis, so they are dropped.
+nhanes_full = nhanes_full.drop(var_full_pri_0['Variable'], axis = 1)
+nhanes_full = nhanes_full.drop(var_full_pri_2['Variable'], axis = 1)
+nhanes_full = nhanes_full.drop(var_full_pri_3['Variable'], axis = 1)
 
 
-#Read output of first data cleanup
-nhanes_full = pd.read_pickle('../Data/nhanes_full_read.pkl')
-nhanes_full_cols = nhanes_full.columns.values.tolist()
-
+'''
+Step: Create age table and eathome table for merging in after aggregation
+Creates a table of unique values with each participant and their age
+'''
 #Extract age for each participant
 age_table = nhanes_full[['SEQN','age']]
 age_table = age_table.drop_duplicates()
 
-#Create a list of all the FPED columns, removing the "DR1I_" prefix. 
-fped_cols = []
-fped_cols_meal_key = []
-for i in range(len(nhanes_full_cols)):
-    if (re.match(r"DR1I_", nhanes_full_cols[i])):
-        fped_cols.append(re.findall(r"DR1I_(.*)", nhanes_full_cols[i])[0])
-        fped_cols_meal_key.append(re.findall(r"DR1I_(.*)", nhanes_full_cols[i])[0])
 
 #Define the dataframe join key for pulling other food items consumed with seafood
 meal_key = ['SEQN', 'DR1.030Z', 'DR1.020']
-
-#Add meal key to FPED cols
-fped_cols_meal_key.extend(meal_key)
-
-#Add calories to FPED cols
-fped_cols_meal_key.extend(['DR1IKCAL'])
-
-#Remove the "DR1I_" prefix for simplification
-nhanes_full.columns = nhanes_full.columns.str.replace('^DR1I_', '')
-
-
-#Keep FPED cols,calories, and meal key
-nhanes_full_filtered = nhanes_full[fped_cols_meal_key]
-#Add calories to aggregated variables
-fped_cols.extend(['DR1IKCAL'])
-#Group by meal key and aggregate on FPED columns
-nff_agg = nhanes_full_filtered.groupby(meal_key)[fped_cols].sum()
 
 #Create dataframe that contains the eathome variable
 df_eathome_key = ['SEQN', 'DR1.030Z', 'DR1.020', 'eathome']
@@ -57,15 +55,56 @@ df_eathome = df_eathome.groupby(meal_key, as_index=False)['eathome'].sum()
 df_eathome['eathome'] = (df_eathome['eathome'] >= 1).astype(int)
 
 
+'''
+Step: Perform aggregation. 
+Create alist of variables that is required.
+Filter the dataframe on that list and perform aggregation
+'''
+
+#Obtain list of all columns
+nhanes_full_cols = nhanes_full.columns.values.tolist()
+
+#Create a list of all the FPED columns, removing the "DR1I_" prefix. 
+#Create a list of all columns that will be used for meal level aggregation
+fped_cols = []
+df_agg_columns = []
+for i in range(len(nhanes_full_cols)):
+    if (re.match(r"DR1I_", nhanes_full_cols[i])):
+        fped_cols.append(re.findall(r"DR1I_(.*)", nhanes_full_cols[i])[0])
+        df_agg_columns.append(re.findall(r"DR1I_(.*)", nhanes_full_cols[i])[0])
+
+
+
+#Add meal key to aggregation cols
+df_agg_columns.extend(meal_key)
+#Add calories to aggregation cols
+df_agg_columns.extend(['DR1IKCAL'])
+
+
+#Remove the "DR1I_" prefix for simplification
+nhanes_full.columns = nhanes_full.columns.str.replace('^DR1I_', '')
+
+#Keep FPED cols,calories, and meal key
+nhanes_full= nhanes_full[df_agg_columns]
+
+#Group by meal key and aggregate on FPED columns
+nhanes_full = nhanes_full.groupby(meal_key).sum()
+
+
+'''
+Step: Merge in the participant age and eathome tables
+
+'''
+
 #Merge in the eathome variable
-df_final = nff_agg.merge(df_eathome, how='left', on=meal_key)
+nhanes_full = nhanes_full.merge(df_eathome, how='left', on=meal_key)
 #Merge in the participant's age
-df_final = df_final.merge(age_table, how='left', on=['SEQN'])
+nhanes_full = nhanes_full.merge(age_table, how='left', on=['SEQN'])
 
 
-#Determine if the meal has seafood in it. If yes, variable = 1, 0 otherwise
-df_final['seafood_meal'] = (df_final['PF_SEAFD_TOT'] >= 1).astype(int)
-
+'''
+Step: Keep only meals that are in the lunch or dinner category
+'''
 
 #Create meal name variable based on lookup, mapping from CDC source
 meal_name_lookup = {1: 'Breakfast', 2: 'Lunch', 3: 'Dinner', 4: 'Supper', 5:'Brunch', 6:'Snack',
@@ -74,14 +113,48 @@ meal_name_lookup = {1: 'Breakfast', 2: 'Lunch', 3: 'Dinner', 4: 'Supper', 5:'Bru
                     16: 'Botana', 17: 'Bocadillo', 18: 'Tentempie', 19:'Bebida', 91: 'Other'}
 
 #Add meal name to dataframe
-df_final['Meal_Name'] = df_final['DR1.030Z'].map(meal_name_lookup)
+nhanes_full['Meal_Name'] = nhanes_full['DR1.030Z'].map(meal_name_lookup)
 
 #Obtain the meals of interest
 meal_name_filter = ['Lunch', 'Dinner', 'Supper', 'Brunch', 'Almuerzo', 'Comida', 'Cena', 'Enter comida']
 
 #Keep only the meals of interest
-df_final = df_final[df_final['Meal_Name'].isin(meal_name_filter)]
+nhanes_full = nhanes_full[nhanes_full['Meal_Name'].isin(meal_name_filter)]
+
+
+'''
+Step: Create seafood meal variable, to categorize if meal has seafood
+'''
+
+#Determine if the meal has seafood in it. If yes, variable = 1, 0 otherwise
+nhanes_full['seafood_meal'] = (nhanes_full['PF_SEAFD_TOT'] > 0).astype(int)
+
+
+'''
+Step: Drop meals that are 0 calories
+'''
+
+nhanes_full = nhanes_full[nhanes_full['DR1IKCAL']>0]
+
+
+'''
+Step: Create a meal energy variable
+'''
+
+#Create meal energy category based on quantiles from KCAL
+nhanes_full.loc[nhanes_full['DR1IKCAL'] < nhanes_full['DR1IKCAL'].describe()['25%'], 'meal_energy'] = "Low"
+nhanes_full.loc[(nhanes_full['DR1IKCAL'] > nhanes_full['DR1IKCAL'].describe()['25%']) 
+       & (nhanes_full['DR1IKCAL'] < nhanes_full['DR1IKCAL'].describe()['50%']), 'meal_energy'] = "Medium-Low"
+nhanes_full.loc[(nhanes_full['DR1IKCAL'] > nhanes_full['DR1IKCAL'].describe()['50%']) 
+       & (nhanes_full['DR1IKCAL'] < nhanes_full['DR1IKCAL'].describe()['75%']), 'meal_energy'] = "Medium-High"
+nhanes_full.loc[nhanes_full['DR1IKCAL'] > nhanes_full['DR1IKCAL'].describe()['75%'], 'meal_energy'] = "High"
+
+
+
+'''
+Step: Save the pre-processed data
+'''
 
 #Save pre-processed data
-df_final.to_pickle('../Data/nhanes_full_pre_proc.pkl')
-df_final.to_csv('../Data/nhanes_full_pre_proc.csv')
+nhanes_full.to_pickle('../Data/nhanes_full_pre_proc.pkl')
+nhanes_full.to_csv('../Data/nhanes_full_pre_proc.csv')
