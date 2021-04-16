2916d6f3953d9d1bc33c183bfcd67326aed62e46
jorid14 <jorid.topi@gmail.com>
2021-04-16 16:49:27 -0400

    data pre-processing modifications and report

--- Scripts/nhanes_full_pre_proc.py
+++ Scripts/nhanes_full_pre_proc.py
@@ -34,6 +34,7 @@ nhanes_full = nhanes_full.drop(var_full_pri_2['Variable'], axis = 1)
 nhanes_full = nhanes_full.drop(var_full_pri_3['Variable'], axis = 1)
 
 
+
 '''
 Step: Create age table and eathome table for merging in after aggregation
 Creates a table of unique values with each participant and their age
@@ -53,6 +54,24 @@ df_eathome = nhanes_full[df_eathome_key]
 df_eathome = df_eathome.groupby(meal_key, as_index=False)['eathome'].sum()
 #If eathome was 0, remains 0. Otherwise, convert it to 1
 df_eathome['eathome'] = (df_eathome['eathome'] >= 1).astype(int)
+
+
+'''
+Step: Create a table with seafood species per meal.
+If a meal has more than one species, keep the one with higher PF_SEAFD_TOT value and drop others
+Each seafood meal then has one type of species for a target
+'''
+
+#Extract observations that have a seafood species
+df_sf_species = nhanes_full[nhanes_full['species'].notna()]
+#Obtain the unique meals + species
+df_sf_species= df_sf_species[['SEQN', 'DR1.030Z', 'DR1.020', 'DR1I_PF_SEAFD_TOT', 'species']]
+#Find the species with higher PF content within a meal, if there are multiple species per meal
+df_sf_species = df_sf_species.groupby(['SEQN', 'DR1.030Z', 'DR1.020', 'species'], sort=False)['DR1I_PF_SEAFD_TOT'].max()
+df_sf_species = df_sf_species.reset_index()
+df_sf_species = df_sf_species.drop(['DR1I_PF_SEAFD_TOT'], axis=1)
+#Drop duplicates
+df_sf_species = df_sf_species.drop_duplicates(['SEQN', 'DR1.030Z', 'DR1.020', 'species'])
 
 
 '''
@@ -92,7 +111,7 @@ nhanes_full = nhanes_full.groupby(meal_key).sum()
 
 
 '''
-Step: Merge in the participant age and eathome tables
+Step: Merge in the participant age, eathome, and species tables
 
 '''
 
@@ -100,6 +119,8 @@ Step: Merge in the participant age and eathome tables
 nhanes_full = nhanes_full.merge(df_eathome, how='left', on=meal_key)
 #Merge in the participant's age
 nhanes_full = nhanes_full.merge(age_table, how='left', on=['SEQN'])
+#Merge in the seafood species
+nhanes_full = nhanes_full.merge(df_sf_species, how='left', on=meal_key)
 
 
 '''
@@ -131,10 +152,37 @@ nhanes_full['seafood_meal'] = (nhanes_full['PF_SEAFD_TOT'] > 0).astype(int)
 
 
 '''
-Step: Drop meals that are 0 calories
+Step: Meals that contain both meat and seafood
+If meals contain both meat and sefood, then compute the ratio of seafood quantity to meat.
+If the ratio is higher than a threshold, classify as seafood meal
+If the ratio is lower than a threshold, classify as a non-seafood meal
+If the ratio is in a grey area, or between above thresholds, drop that meals altogether.
+Threshold is 1.5 seafood quantity to 1 meat for seafood class and inverse for non-seafood class
 '''
 
-nhanes_full = nhanes_full[nhanes_full['DR1IKCAL']>0]
+#Create a separate dataframe equal to nhanes full
+df_meat_sfd = nhanes_full
+#Create meat PF variable, by subtracting the seafood total from the MPS Total
+df_meat_sfd['PF_MPS_TOTAL_NSFD'] = df_meat_sfd['PF_MPS_TOTAL'] - df_meat_sfd['PF_SEAFD_TOT']
+#Find meals that contain both meat and seafood
+df_meat_sfd = df_meat_sfd[(df_meat_sfd['PF_MPS_TOTAL_NSFD']>0)&(df_meat_sfd['PF_SEAFD_TOT']>0)]
+#Compute the seafood to meat ratio
+df_meat_sfd['seafood_meat_ratio'] = df_meat_sfd['PF_MPS_TOTAL_NSFD']/df_meat_sfd['PF_SEAFD_TOT']
+#Create a temporary class to classify based on the ratio
+df_meat_sfd.loc[df_meat_sfd['seafood_meat_ratio']>1.5, 'seafood_class'] = "Yes"
+df_meat_sfd.loc[df_meat_sfd['seafood_meat_ratio']<(1/1.5), 'seafood_class'] = "No"
+df_meat_sfd.loc[df_meat_sfd['seafood_class'].isna(), 'seafood_class'] = "Maybe"
+#Only keep the meal key and the new class, drop duplicates
+df_meat_sfd = df_meat_sfd[['SEQN', 'DR1.030Z', 'DR1.020', 'seafood_class']]
+df_meat_sfd = df_meat_sfd.drop_duplicates(['SEQN', 'DR1.030Z', 'DR1.020', 'seafood_class'])
+#Merge the new class into the dataframe 
+nhanes_full = nhanes_full.merge(df_meat_sfd, how='left', on=meal_key)
+#Remove meals that are in the grey area between thresholds
+nhanes_full = nhanes_full[nhanes_full['seafood_class'] != 'Maybe']
+#Recode the seafood class based on the results from these observations
+nhanes_full.loc[nhanes_full.seafood_class == 'No', ['seafood_meal']] = 0
+#Drop the temporary class
+nhanes_full = nhanes_full.drop(['seafood_class'], axis=1)
 
 
 '''
@@ -150,6 +198,27 @@ nhanes_full.loc[(nhanes_full['DR1IKCAL'] > nhanes_full['DR1IKCAL'].describe()['5
 nhanes_full.loc[nhanes_full['DR1IKCAL'] > nhanes_full['DR1IKCAL'].describe()['75%'], 'meal_energy'] = "High"
 
 
+'''
+Step: Drop meals that are 0 calories
+'''
+nhanes_full = nhanes_full[nhanes_full['DR1IKCAL']>0]
+
+'''
+Step: Drop meals of participants below the age of 18
+'''
+nhanes_full = nhanes_full[nhanes_full['age'] >= 18]
+
+'''
+Step: Drop meals that are not home made
+'''
+nhanes_full = nhanes_full[nhanes_full['eathome']==1]
+
+'''
+Step: Drop meals that are vegeterian
+'''
+#Drop meals that do not contain any type of meat or seafood
+nhanes_full = nhanes_full[nhanes_full['PF_MPS_TOTAL'] > 0]
+
 
 '''
 Step: Save the pre-processed data
@@ -158,3 +227,5 @@ Step: Save the pre-processed data
 #Save pre-processed data
 nhanes_full.to_pickle('../Data/nhanes_full_pre_proc.pkl')
 nhanes_full.to_csv('../Data/nhanes_full_pre_proc.csv')
+
+
